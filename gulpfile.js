d810d41ef963ec2c8ffe8344d5cca94d2c9647f8
alexzhong22c <535377683@qq.com>
2017-10-09 18:45:45 +0800

    抄成sass之后，第一次跑gulp，修复小bug

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,13 @@ var gulp = require('gulp')
 var sass = require('gulp-sass')
 
 gulp.task('sass', function () {
-  return gulp.src('sass/**/*.scss')
+  return gulp.src('sass/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('dist/css'))
 })
+
+gulp.task('watch', function () {
+  gulp.watch('sass/*.scss', ['sass'])
+})
+
+gulp.task('default', ['sass', 'watch'])
